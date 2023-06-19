59024202ccdffc9b5d6d60a13449399ea42b859c
Nicolas Knoebber <knoebber@gmail.com>
2023-06-19 09:49:37 -0700

    Add wind/rain

--- weather.py
+++ weather.py
@@ -1,22 +1,30 @@
-import time
+#!/usr/bin/python
+
 import json
+import os
+import time
+
+from typing import NamedTuple
 from urllib.request import Request
 from urllib.request import urlopen
 
-from typing import NamedTuple
-
 from weatherhat import WeatherHAT
 
+UPDATE_PERIOD_SECONDS = 10
 PREC = 3
 LOCAL_PURPLE_URL = 'http://nicolass-macbook-air.local:4000'
 PROD_PURPLE_URL = 'https://mypurplewebsite.com'
 
-PURPLE_URL = LOCAL_PURPLE_URL
+def get_purple_url():
+    if os.getenv('PURPLE_ENV') == 'PROD':
+        return PROD_PURPLE_URL
+    else:
+        return LOCAL_PURPLE_URL
 
 sensor = WeatherHAT()
 
 def post(url: str, payload: dict):
-    json_str = json.dumps({"weather_snapshot": payload})
+    json_str = json.dumps({'weather_snapshot': payload})
     request = Request(url, data=json_str.encode('utf-8'), method='POST')
     request.add_header('Content-Type', 'application/json')
     with urlopen(request) as response:
@@ -24,31 +32,42 @@ def post(url: str, payload: dict):
 
 class WeatherData(NamedTuple):
     humidity: float
+    is_wind_and_rain_updated: bool
     pressure: float
+    rain_millimeters: float
     temperature: float
     unix_timestamp: int
+    update_period_seconds: int
+    wind_direction_degrees: int
+    wind_speed_ms: float
 
     def broadcast(self):
-        post(PURPLE_URL + '/api/weather_snapshots/broadcast', self._asdict())
+        post(get_purple_url() + '/api/weather_snapshots/broadcast', self._asdict())
 
 def get_data():
-    sensor.update(interval=5.0)
     return WeatherData(
         humidity=round(sensor.humidity, PREC),
+        is_wind_and_rain_updated=sensor.updated_wind_rain,
         pressure=round(sensor.pressure, PREC),
+        rain_millimeters=round(sensor.rain_total, PREC),
         temperature=round((sensor.temperature*1.8) + 32, PREC),
         unix_timestamp=int(time.time()),
+        update_period_seconds=UPDATE_PERIOD_SECONDS,
+        wind_direction_degrees=int(sensor.wind_direction),
+        wind_speed_ms=round(sensor.wind_speed, PREC) or 0,
     )
 
 def run():
     while True:
+        weather_data = None
         try:
+            sensor.update(interval=UPDATE_PERIOD_SECONDS)
             weather_data = get_data()
             weather_data.broadcast()
         except Exception as e:
-            print('failed to send data', e)
+            print('failed to send', weather_data, e)
+        finally:
+            time.sleep(1)
 
-        time.sleep(1)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
